305b76adce4d74704dff61f3fe1ee0c80f6a6595
dave-8bit <223476010+dave-8bit@users.noreply.github.com>
2026-07-13 20:10:03 +0100

    feat: add support for ports 5173 in CORS configuration

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -20,6 +20,8 @@ app.use(
       "http://127.0.0.1:5500",
       "http://localhost:5500",
       "http://localhost:3000",
+      "http://localhost:5173",
+      "http://127.0.0.1:5173",
     ],
     methods: ["GET", "POST", "OPTIONS"],
     allowedHeaders: ["Content-Type"],
